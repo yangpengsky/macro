245c1e1944c5159235466a1373efd35df7f81733
yangpeng <376562891@qq.com>
2022-09-01 16:23:17 +0800

    杨鹏第一次提交

--- OCP/src/main/java/com/macro/ocp/service/impl/ProductServiceImpl.java
+++ OCP/src/main/java/com/macro/ocp/service/impl/ProductServiceImpl.java
@@ -1,12 +1,15 @@
 package com.macro.ocp.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.macro.ocp.pojo.Product;
 import com.macro.ocp.service.ProductService;
+import com.macro.ocp.mapper.ProductMapper;
 import org.springframework.stereotype.Service;
 
 /**
-* @author Admin
+* @author 37656
 * @description 针对表【product(产品表)】的数据库操作Service实现
-* @createDate 2022-09-01 14:44:42
+* @createDate 2022-09-01 16:19:50
 */
 @Service
 public class ProductServiceImpl implements ProductService{
