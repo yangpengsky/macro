a8be71793c0513822dcaf3999f832d94cf1d5752
huangguo <1461188320@qq>
2022-09-01 15:33:17 +0800

    黄果第一次提交

---
+++ OCP/src/main/java/com/macro/ocp/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.macro.ocp.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author meng
+* @description 针对表【product(产品表)】的数据库操作Mapper
+* @createDate 2022-09-01 15:27:40
+* @Entity com.macro.ocp.pojo.Product
+*/
+public interface ProductMapper extends BaseMapper {
+
+
+}
