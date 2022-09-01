637b900c9729581b94eb33d9146842af9663e090
kein <zhangkein@foxmail.com>
2022-09-01 11:38:18 +0800

    初始化的项目

---
+++ OCP/src/main/java/com/macro/ocp/contraller/RoleContraller.java
@@ -0,0 +1,18 @@
+package com.macro.ocp.contraller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * role的控制层
+ * @author zhangkein
+ */
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/role")
+public class RoleContraller {
+    //127.0.0.1:8080/role/getRoleByNo
+
+
+}
---
+++ OCP/src/main/java/com/macro/ocp/service/impl/OrdersServiceImpl.java
@@ -0,0 +1,14 @@
+package com.macro.ocp.service.impl;
+
+import com.macro.ocp.service.OrdersService;
+import org.springframework.stereotype.Service;
+
+/**
+* @author Admin
+* @description 针对表【orders(订单表)】的数据库操作Service实现
+* @createDate 2022-09-01 11:01:29
+*/
+@Service
+public class OrdersServiceImpl implements OrdersService{
+
+}
