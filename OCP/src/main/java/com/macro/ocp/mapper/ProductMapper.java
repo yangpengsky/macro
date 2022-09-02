7655f3dd6a402ea0c3fbf602e376975619e7b8b6
huangguo <1461188320@qq>
2022-09-02 14:33:35 +0800

    好好干

---
+++ OCP/src/main/java/com/macro/ocp/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.macro.ocp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.macro.ocp.pojo.Product;
+
+/**
+* @author meng
+* @description 针对表【product(产品表)】的数据库操作Mapper
+* @createDate 2022-09-01 14:07:38
+* @Entity com.macro.ocp.pojo.Product
+*/
+public interface ProductMapper extends BaseMapper<Product> {
+
+
+}
---
+++ OCP/src/main/java/com/macro/ocp/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.macro.ocp.service;
+
+
+/**
+* @author meng
+* @description 针对表【product(产品表)】的数据库操作Service
+* @createDate 2022-09-01 14:07:38
+*/
+public interface ProductService {
+
+}
