26167336c20957f9d21d4a7009e0c93dd842efea
yangpeng <376562891@qq.com>
2022-09-02 08:27:43 +0800

    yyyydasd

---
+++ OB/src/main/java/com/macro/ob/service/impl/TestServiceImpl.java
@@ -0,0 +1,6 @@
+package com.macro.ob.service.impl;
+
+import com.macro.ob.service.TestService;
+
+public class TestServiceImpl implements TestService {
+}
---
+++ OCP/src/main/java/com/macro/ocp/controller/TestController.java
@@ -0,0 +1,4 @@
+package com.macro.ocp.controller;
+
+public class TestController {
+}
