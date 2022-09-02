1e959f9f45b74a5b6088142e631aed683d2c5f06
kein <zhangkein@foxmail.com>
2022-09-02 15:14:22 +0800

    OCP开始添加订单增加功能

---
+++ OCP/src/main/java/com/macro/ocp/mapper/OrdersMapper.java
@@ -0,0 +1,15 @@
+package com.macro.ocp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.macro.ocp.pojo.Orders;
+
+/**
+* @author zhangkein
+* @description 针对表【orders(订单表)】的数据库操作Mapper
+* @createDate 2022-09-02 14:05:25
+* @Entity com.macro.ocp.pojo.Orders
+*/
+public interface OrdersMapper extends BaseMapper<Orders> {
+
+
+}
---
+++ OCP/src/main/java/com/macro/ocp/service/OrdersService.java
@@ -0,0 +1,9 @@
+package com.macro.ocp.service;
+/**
+* @author zhangkein
+* @description 针对表【orders(订单表)】的数据库操作Service
+* @createDate 2022-09-02 14:05:25
+*/
+public interface OrdersService{
+
+}
